696c8a40da2fa0467387058d2c907cfd79d2c21a
Ynryo <ewenatideklein@gmail.com>
2024-04-12 16:47:01 +0200

    commit

--- assets/src/panels-assets/tabs.php
+++ assets/src/panels-assets/tabs.php
@@ -32,31 +32,3 @@
     </div>
     <script src="/assets/js/modals.js"></script>
 </div>
-<div class="main-container">
-    <div class="top">
-        <div class="left">
-            <h1>Tableau de bord</h1>
-            <span class="material-symbols-rounded panel-top">
-                chevron_right
-            </span>
-            <div class="rounded_type_name handi">
-                <span class="material-symbols-rounded">accessible</span>
-                <h2><?php echo $_SESSION["ho_associated_firstname"] . " " . $_SESSION["ho_associated_name"] ?></h2>
-            </div>
-        </div>
-    </div>
-    <div class="main-content">
-        <div>
-            <p>Grid 1</p>
-        </div>
-        <div>
-            <p>Grid 2</p>
-        </div>
-        <div>
-            <p>Grid 3</p>
-        </div>
-        <div>
-            <p>Grid 4</p>
-        </div>
-    </div>
-</div>
--- family/index.php
+++ family/index.php
@@ -13,6 +13,34 @@
 <body>
     <div class="main">
         <?php include(dirname(__FILE__, 2) . "/assets/src/panels-assets/tabs.php") ?>
+        <div class="main-container">
+            <div class="top">
+                <div class="left">
+                    <h1>Tableau de bord</h1>
+                    <span class="material-symbols-rounded panel-top">
+                        chevron_right
+                    </span>
+                    <div class="rounded_type_name handi">
+                        <span class="material-symbols-rounded">accessible</span>
+                        <h2><?php echo $_SESSION["ho_associated_firstname"] . " " . $_SESSION["ho_associated_name"] ?></h2>
+                    </div>
+                </div>
+            </div>
+            <div class="main-content">
+                <div class="frame bpm">
+                    <h2>Battement par minute (BPM)</h2>
+                </div>
+                <div class="frame sp02">
+                    <h2>Taux d'oxygène dans le sang (Sp02)</h2>
+                </div>
+                <div class="frame sleep">
+                    <h2>Sommeil</h2>
+                </div>
+                <div class="frame medication">
+                    <h2>Médicaments</h2>
+                </div>
+            </div>
+        </div>
     </div>
 </body>
 
