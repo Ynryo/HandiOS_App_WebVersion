03f3afb1914b5d78ec461101327148c9c5ab526f
Ynryo <ewenatideklein@gmail.com>
2024-04-16 22:49:02 +0200

    commit

--- assets/src/header.php
+++ assets/src/header.php
@@ -11,6 +11,8 @@
 if ((str_contains($_SERVER["REQUEST_URI"], "/family/") || str_contains($_SERVER["REQUEST_URI"], "/nurse/") || str_contains($_SERVER["REQUEST_URI"], "/senior/") || str_contains($_SERVER["REQUEST_URI"], "/handicap/"))) {
     echo "<link rel=\"stylesheet\" href=\"/assets/css/panels/sidebar.css\">
     <link rel=\"stylesheet\" href=\"/assets/css/panels/main-content.css\">
+    <link rel=\"stylesheet\" href=\"/assets/css/panels/frames/sleep.css\">
+    <link rel=\"stylesheet\" href=\"/assets/css/panels/frames/medication.css\">
     <link rel=\"stylesheet\" href=\"https://fonts.googleapis.com/css2?family=Material+Symbols+Rounded:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200\" />
     <script src=\"https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.4/Chart.js\"></script>";
 } else {
--- family/index.php
+++ family/index.php
@@ -1,18 +1,18 @@
-<?php include(dirname(__FILE__, 2) . "/assets/src/session.php") ?>
+<?php include (dirname(__FILE__, 2) . "/assets/src/session.php") ?>
 <!DOCTYPE html>
 <html lang="fr">
 
 <head>
     <title>Handi'OS</title>
     <meta property="og:site_name" content="Handi'OS">
-    <?php include(dirname(__FILE__, 2) . "/assets/src/header.php") ?>
+    <?php include (dirname(__FILE__, 2) . "/assets/src/header.php") ?>
     <link rel="stylesheet" type="text/css" href="/assets/css/main.css">
     <link rel="stylesheet" type="text/css" href="/assets/css/buttons.css">
 </head>
 
 <body>
     <div class="main">
-        <?php include(dirname(__FILE__, 2) . "/assets/src/panels-assets/tabs.php") ?>
+        <?php include (dirname(__FILE__, 2) . "/assets/src/panels-assets/tabs.php") ?>
         <div class="main-container">
             <div class="top">
                 <div class="left">
@@ -22,7 +22,8 @@
                     </span>
                     <div class="rounded_type_name handi">
                         <span class="material-symbols-rounded">accessible</span>
-                        <h2><?php echo $_SESSION["ho_associated_firstname"] . " " . $_SESSION["ho_associated_name"] ?></h2>
+                        <h2><?php echo $_SESSION["ho_associated_firstname"] . " " . $_SESSION["ho_associated_name"] ?>
+                        </h2>
                     </div>
                 </div>
             </div>
@@ -35,23 +36,68 @@
                 </div>
                 <div class="frame sleep">
                     <h2>Sommeil</h2>
-                    <div class="frame-content">
+                    <div class="frame-content content-rows">
+                        <div class="rem-sleep">
+                            <h3>01h04</h3>
+                            <h4>Sommeil paradoxal</h4>
+                        </div>
                         <div class="light-sleep">
-                            <h3>04h24</h3>
+                            <h3>03h49</h3>
                             <h4>Sommeil léger</h4>
                         </div>
                         <div class="total-sleep-time">
-                            <h3>08h54</h3>
+                            <h3>06h02</h3>
                             <h4>Temps de sommeil total</h4>
                         </div>
                         <div class="deep-sleep">
-                            <h3>01h43</h3>
-                            <h4>Sommeil léger</h4>
+                            <h3>01h09</h3>
+                            <h4>Sommeil profond</h4>
+                        </div>
+                        <div class="awake">
+                            <h3>00h59</h3>
+                            <h4>Eveillé</h4>
                         </div>
                     </div>
                 </div>
                 <div class="frame medication">
                     <h2>Médicaments</h2>
+                    <div class="frame-content content-column">
+                        <div class="content-rows">
+                            <div class="morning">
+                                <h3>Matin</h3>
+                                <h4>Pris</h4>
+                            </div>
+                            <div class="lunch">
+                                <h3>Midi</h3>
+                                <h4>Pris</h4>
+                            </div>
+                            <div class="afternoon">
+                                <h3>Après-midi</h3>
+                                <h4>Pas pris</h4>
+                            </div>
+                            <div class="dinner">
+                                <h3>Diner</h3>
+                                <h4>A venir</h4>
+                            </div>
+                            <div class="night">
+                                <h3>Nuit</h3>
+                                <h4>A venir</h4>
+                            </div>
+                        </div>
+                        <div class="content-rows">
+                            <div class="empty-line"></div>
+                            <div class="circle taken"></div>
+                            <div class="line full taken"></div>
+                            <div class="circle taken"></div>
+                            <div class="line full taken"></div>
+                            <div class="circle nottaken"></div>
+                            <div class="line half nottaken"></div>
+                            <div class="circle notyet"></div>
+                            <div class="line full notyet"></div>
+                            <div class="circle notyet"></div>
+                            <div class="empty-line"></div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
